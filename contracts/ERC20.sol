244575c3b2f0f03c246bf08f03c15a891c647781
Harris <acyclicgraphs@gmail.com>
2022-06-28 11:59:08 -0400

    Update ERC20.sol

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -141,9 +141,28 @@ abstract contract ERC20 {
                 )
             );
     }
+    // internal mint and burn logic
+    function _mint(address to, uint256 amount) internal virtual {
+        totalSupply += amount;
 
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to] += amount;
+        }
 
+        emit Transfer(address(0), to, amount);
+    }
 
+    function _burn(address from, uint256 amount) internal virtual {
+        balanceOf[from] -= amount;
 
+        // Cannot underflow because a user's balance
+        // will never be larger than the total supply.
+        unchecked {
+            totalSupply -= amount;
+        }
 
+        emit Transfer(from, address(0), amount);
+    }
 }
