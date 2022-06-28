3418084d7c7f409ed48dce17a678a0b91163cb22
Harris <acyclicgraphs@gmail.com>
2022-06-28 09:44:23 -0400

    Update Escrow.sol

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,5 +1,27 @@
-pragma solidity 0.8.10;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
 
 contract Escrow {
 
+  address public funder;
+  
+  address public recipient;
+  
+  function fund(address counterparty) public payable {
+    recipient = counterparty;
+    
+    funder = msg.sender;
+  }
+
+  function release() public payable {
+    if (msg.sender == funder) {
+    
+      payable(recipent).transfer(address(this).balance);
+  }
+}
+
+  function getBalance() public view returns (uint) {
+    return address(this).balance;
+  }
 }
